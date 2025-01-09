052abba8412e63840c5cd49f1a62bc9d05125e08
Rohan Marar <97607649+Rohan5manza@users.noreply.github.com>
2025-01-09 20:59:00 +0530

    Delete src/app/components/SplineEmbed.tsx

--- src/app/components/SplineEmbed.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from "../page.module.css";
-import Spline from "@splinetool/react-spline";
-
-export default function SplineEmbed({ sceneUrl, className }: { sceneUrl: string; className: string }) {
-    return <Spline className={className} scene={sceneUrl} />;
-}
