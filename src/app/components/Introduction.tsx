74f5e60b0a6a44ebabc6ee0ea14820af765dffb1
Rohan Marar <97607649+Rohan5manza@users.noreply.github.com>
2025-02-13 14:00:41 +0530

    Update Introduction.tsx

--- src/app/components/Introduction.tsx
+++ src/app/components/Introduction.tsx
@@ -5,14 +5,14 @@ export default function Introduction() {
     return (
         <div className={styles.elementsSH}>
             <div className={styles.content}>
-                <h2>Turning desires into Reality with AI Solutions </h2>
+                <h2>Empowering limitless innovation through your private, on‑premise multimodal AI lab. </h2>
                 <h3>
   <p></p>
 
-                    <li>At TensorBuilds, we believe in the transformative power of artificial intelligence. We are an idea factory that execute upon valuable concepts.</li>
-                    <li>We build premium D2C SaaS products to solve the needs of people across different domains. </li>
-                    <li>AI is a hyper-competitive market. Talent determines the quality of the products, and separates noise from what really matters. This is why our mission is to hire talented programmers to provide a product/market fit of real value. </li>
-                    <p>Our goal is to be a unified place for next-gen AI cloud apps.</p>
+                    <li>At TensorBuilds, we believe in the transformative power of AI. We are an idea factory that executes upon valuable concepts.</li>
+                    <li>Our star product is BulkBites : A multimodal AI app for food calorie counting, recipe and workout assistant for bodybuilders.</li>
+                    <p>Our future goal : Foundational models made small to fit on consumer hardware. Multimodal AI is the future.</p>
+                    <p>We believe AI should be available for tinkerers. Each person should own his AI privately and securely on his hardware.</p>
                 </h3>
             </div>
             <img src='astronaut.png'></img>
