5ecb25a2811a9d91a5862dd885262aded524199f
Rohan Marar <97607649+Rohan5manza@users.noreply.github.com>
2025-02-22 16:47:52 +0530

    Update Innovations.tsx

--- src/app/components/Innovations.tsx
+++ src/app/components/Innovations.tsx
@@ -4,29 +4,15 @@ import styles from "../page.module.css";
 export default function Innovations() {
     return (
         <>
-        <h2 className={styles.heading}>Pursuing Innovation</h2>
-        <h3 className={styles.content}>Along with products and services, we drive innovation in AI by building our own new models, software dev packages, libraries, and documentation.
-
-We launch python packages as toolkit suite for ML practitioners and researchers, and we strive for breakthroughs by launching new customized AI models.</h3>
+        <h2 className={styles.heading}>BulkBites</h2>
+        <h3 className={styles.content}>Mobile app with computer vision for food calorie estimation, recipe cooking and workout assistant.</h3>
         <div className={styles.compare}>
             <div className={styles.card1}>
-                <h2>Turbotune</h2>
+                <h2>Download now</h2>
                 <ol>
-                    <li>We release fine-tuned quantized models tailored for unique use cases and optimized for constrained compute resources.</li>
-                    <li>Our innovative models are supported by proprietary code systems and packages designed to enhance their usability and functionality.</li>
-                    <li>We contribute to the open-source community by making some of our base models freely available for public use.</li>          
+                    <li>CLick here</li> 
                 </ol>
-                <li className={styles.ComingSoon}>Coming Soon..</li>
-            </div>
-            <div className={styles.card2}>
-                <h2>Featurelens</h2>
-                <ol>
-                    <li>The package helps visualize how different prompts impact Large Language Models (LLMs), supporting prompt engineers in refining their prompt strategies.</li>
-                    <li>It provides tools for understanding feature explainability in black box models, allowing for better transparency and insight into how inputs affect outputs.
-                    </li>
-                    <li>Built with PyTorch modules, the package is easy to install via pip and comes with comprehensive documentation for smooth integration into existing workflows.</li>    
-                </ol>
-                <li className={styles.ComingSoon}>Coming Soon..</li>
+                <li className={styles.ComingSoon}>Coming Soon.</li>
             </div>
         </div>
         </>
