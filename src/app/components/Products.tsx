598310ad5e900ebd2e86f863936d0a59280038ab
Rohan Marar <97607649+Rohan5manza@users.noreply.github.com>
2025-02-22 16:13:55 +0530

    Update Products.tsx

--- src/app/components/Products.tsx
+++ src/app/components/Products.tsx
@@ -6,7 +6,7 @@ export default function Products() {
         <div className={styles.elements}>
             <div className={styles.content}>
                 <h2 className={styles.heading}>
-                What do we exactly offer ?
+                Our other product lines (discontinued)  
                 </h2>
                 
                 <div className={styles.gridContainer}>
@@ -23,34 +23,6 @@ export default function Products() {
                         <p className={styles.ProductName}>WealthifyAI</p>
                         <p className={styles.Description}>Leverage advanced AI algorithms and quant strategies to maximize profits for your trading journey. Get insights across all market sections with the help of accurate recommendations.</p>
                         <a className={styles.Status} href='https://wealthifyai-next.vercel.app/'>Access here</a>
-                    </div>
-                    <div className={styles.Item}>
-                    <img src="SynapseIQ.jpg" alt="Personal URL" className={styles.gridImage} />
-                        <p className={styles.Type}>AI-based learning assistant</p>
-                        <p className={styles.ProductName}>SynapseIQ</p>
-                        <p className={styles.Description}>AI based learning education assistant. Multimodal features for syllabus summaries. Perfect for teachers and students.</p>
-                        <a className={styles.Status}>Coming soon</a>
-                    </div>
-                    <div className={styles.Item}>
-                    <img src="BulkBites.jpg" alt="Personal URL" className={styles.gridImage} />
-                        <p className={styles.Type}>CV food app for bodybuilders</p>
-                        <p className={styles.ProductName}>BulkBites</p>
-                        <p className={styles.Description}>Calorie tracker for your food. Get rid of weighing scales. A real-time recipe assistant and workout planner. Upload your physique photos and cooking videos to track progress and get AI insights for your diet and workouts. Perfectly tailored for bodybuilders.</p>
-                        <a className={styles.Status}>Coming soon</a>
-                    </div>
-                    <div className={styles.Item}>
-                    <img src="ContentForge.jpg" alt="Personal URL" className={styles.gridImage} />
-                        <p className={styles.Type}>GenAI content creation</p>
-                        <p className={styles.ProductName}>ContentForge</p>
-                        <p className={styles.Description}>A unified platform for generating content for your use cases, with a mixture of various modalities like audio, video, images and text. Perfect for story writers, animators, and content creators.</p>
-                        <a className={styles.Status}>Coming soon</a>
-                    </div>
-                    <div className={styles.Item}>
-                    <img src="NeuroCanvas.jpg" alt="Personal URL" className={styles.gridImage} />
-                        <p className={styles.Type}>ML project monitoring dashboard</p>
-                        <p className={styles.ProductName}>NeuroCanvas</p>
-                        <p className={styles.Description}>A unified platform for running ML projects, track experiments, save dataset info, training logs, visualize deep learning models, and an IDE/code editor & assistant specially for ML.</p>
-                        <a className={styles.Status}>Coming soon</a>
                     </div>
 
                 </div>
