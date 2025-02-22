d5153f3c38c106e292e5924929ece05a99325413
Rohan Marar <97607649+Rohan5manza@users.noreply.github.com>
2025-02-22 16:11:46 +0530

    Update Introduction.tsx

--- src/app/components/Introduction.tsx
+++ src/app/components/Introduction.tsx
@@ -5,12 +5,12 @@ export default function Introduction() {
     return (
         <div className={styles.elementsSH}>
             <div className={styles.content}>
-                <h2>Empowering limitless innovation through your private, on‑premise multimodal AI models</h2>
+                <h2>Private, on‑premise and customizable multimodal AI models for tinkerers is the future!</h2>
                 <h3>
   <p></p>
 
                     <li>At TensorBuilds, we believe in the transformative power of AI. We are an idea factory that executes upon valuable concepts.</li>
-                    <li>Our star product is BulkBites : A multimodal AI app for food calorie counting, recipe and workout assistant for bodybuilders.</li>
+                    <li>Our star product is BulkBites</li>
                     <p>Our future goal : Foundational models made small to fit on consumer hardware. Multimodal AI is the future.</p>
                     <p>We believe AI should be available for tinkerers. Each person should own his AI privately and securely on his hardware.</p>
                 </h3>
