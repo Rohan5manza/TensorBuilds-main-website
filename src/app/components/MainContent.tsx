3f5ec5c1cc5dd67f9f5256bce811be292b7ca202
Rohan Marar <97607649+Rohan5manza@users.noreply.github.com>
2025-02-22 16:49:52 +0530

    Update MainContent.tsx

--- src/app/components/MainContent.tsx
+++ src/app/components/MainContent.tsx
@@ -5,13 +5,14 @@ import Initiatives from "./Initiatives";
 import styles from "../page.module.css";
 import Products from "./Products";
 import Introduction from "./Introduction";
-
+import BulkBites from "./Bulkbites";
 export default function MainContent() {
     return (
         <main className={styles.main}>
 
             <LandingPage />
             <Introduction />
+            <BulkBites />
             <Products />
             
             <Teams/>
