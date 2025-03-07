503ea873f53ae5c3ce38b60ae4981e0054922b41
Rohan Marar <97607649+Rohan5manza@users.noreply.github.com>
2025-03-07 20:30:55 +0530

    Update Introduction.tsx

--- src/app/components/Introduction.tsx
+++ src/app/components/Introduction.tsx
@@ -5,14 +5,14 @@ export default function Introduction() {
     return (
         <div className={styles.elementsSH}>
             <div className={styles.content}>
-                <h2> Our Code : Private, on‑premise and customizable multimodal AI models for tinkerers is the future!</h2>
+                <h2> Our Code : private, on‑premise and customizable multimodal AI models for tinkerers is the future!</h2>
                 <h3>
   <p></p>
 
                     <li>At TensorBuilds, we believe in the transformative power of AI. We are an idea factory that executes upon valuable concepts.</li>
-                    <p>Our future goal : Foundational models made small to fit on consumer hardware. Multimodal AI is the future.</p>
+                    <p>Our future vision : Foundational models made small to fit on consumer hardware. Multimodal AI is the future.</p>
                     <p>We believe AI should be available for tinkerers. Each person should own his AI privately and securely on his hardware.</p>
-                    <p>This belief guided us to build our star product BulkBites </p>
+                    <p>This philosophy guides us in building our star product BulkBites </p>
                     <p>We started TOTB as a content creation-led growth engine to promote BulkBites. This also allows us to share our technical journey, exciting news or videos for tutorials, and let our creative juices flow! </p>
                 </h3>
             </div>
