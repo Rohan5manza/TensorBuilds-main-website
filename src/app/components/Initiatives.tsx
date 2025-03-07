8b51d8d8207c401b35661ffda2c88cac19fc3e59
Rohan Marar <97607649+Rohan5manza@users.noreply.github.com>
2025-03-07 20:57:55 +0530

    Update Initiatives.tsx

--- src/app/components/Initiatives.tsx
+++ src/app/components/Initiatives.tsx
@@ -5,14 +5,12 @@ export default function Initiatives() {
     return (
         <>
             <h2 className={styles.heading}>Our Initiative</h2>
+            <h2>Think Outside The Box.</h2>
             <div className={styles.imageGrid}>
                 {/* First Image */}
-                <div className={styles.imageContainer}>
-                    <img src="Neuralangelo.png" className={styles.largeImage} alt="Neuralangelo"/>
-                    <div className={styles.overlay}>
-                        <h1>Think Outside The Box.</h1>
+                    <div>
                         <p>Under TOTB, we make tech videos spanning multiple domains.</p>
-                        <p>We focus on turning ideas into products and growing software businesses.</p>
+                        <p>This is our content-creation led growth engine to promote BulkBites, and to share our technical journey along the process in a fun way.</p>
                         <p>Content includes YouTube videos, guides, tutorials, and books as educational resources.</p>
                     </div>
                 </div>
@@ -20,12 +18,6 @@ export default function Initiatives() {
                 {/* Second Image */}
                 <div className={styles.imageContainer}>
                     <img src="NewImage.png" className={styles.largeImage} alt="New Image"/>
-                    <div className={styles.overlay}>
-                        <h1>Innovation & AI.</h1>
-                        <p>We curate and document AI models and innovations while offering peer-reviewed content for accuracy.</p>
-                        <p>Expand AI research and develop practical products to grow the startup.</p>
-                        <p>Providing the latest insights in AI, deep learning, and software engineering.</p>
-                    </div>
                 </div>
             </div>
         </>
