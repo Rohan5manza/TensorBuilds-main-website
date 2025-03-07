c274c32b9675f0df27dac645937cbf07079bf806
Rohan Marar <97607649+Rohan5manza@users.noreply.github.com>
2025-03-07 21:02:24 +0530

    Update Initiatives.tsx

--- src/app/components/Initiatives.tsx
+++ src/app/components/Initiatives.tsx
@@ -4,16 +4,13 @@ import styles from "../page.module.css";
 export default function Initiatives() {
     return (
         <>
-            <h2 className={styles.heading}>Our Initiative</h2>
-            <h2>Think Outside The Box.</h2>
-            <div className={styles.imageGrid}>
+            <h2 className={styles.heading}>Think Outside The Box.</h2>
                 {/* First Image */}
                     <div>
-                        <p>Under TOTB, we make tech videos spanning multiple domains.</p>
+                        <p>Under TOTB initiative, we make tech videos spanning multiple domains.</p>
                         <p>This is our content-creation led growth engine to promote BulkBites, and to share our technical journey along the process in a fun way.</p>
                         <p>Content includes YouTube videos, guides, tutorials, and books as educational resources.</p>
                     </div>
-                </div>
 
                 {/* Second Image */}
                 <div className={styles.imageContainer}>
