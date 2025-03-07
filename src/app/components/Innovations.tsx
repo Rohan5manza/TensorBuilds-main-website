6f06a21a58d7bd92e7d6ff4c040a39946d07f7cd
Rohan Marar <97607649+Rohan5manza@users.noreply.github.com>
2025-03-07 20:47:39 +0530

    Update Innovations.tsx

--- src/app/components/Innovations.tsx
+++ src/app/components/Innovations.tsx
@@ -5,15 +5,9 @@ export default function Innovations() {
     return (
         <>
         <h2 className={styles.heading}>BulkBites</h2>
-        <h3 className={styles.content}>Mobile app with computer vision for food calorie estimation, recipe cooking and workout assistant.</h3>
-        <div className={styles.compare}>
-            <div className={styles.card1}>
-                <h2>Download now</h2>
-                <ol>
-                    <li>CLick here</li> 
-                </ol>
-                <li className={styles.ComingSoon}>Coming Soon.</li>
-            </div>
+        <h3 className={styles.content}>An upcoming Computer vision Mobile app for food recognition and calorie/macronutrient estimation, designed to help bodybuilders with their workouts, and be an assitant for cooking recipes.</h3>
+        <p>Coming soon.</p>
+            
         </div>
         </>
     );
