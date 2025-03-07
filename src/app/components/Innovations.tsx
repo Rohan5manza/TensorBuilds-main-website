8168709c3393d0077aac84978d8262e81b95ee3a
Rohan Marar <97607649+Rohan5manza@users.noreply.github.com>
2025-03-07 21:04:44 +0530

    Update Innovations.tsx

--- src/app/components/Innovations.tsx
+++ src/app/components/Innovations.tsx
@@ -1,14 +1,2 @@
 "use client";
 import styles from "../page.module.css";
-
-export default function Innovations() {
-    return (
-        <>
-        <h2 className={styles.heading}>BulkBites</h2>
-        <p>An upcoming Computer vision Mobile app for food recognition and calorie/macronutrient estimation, designed to help bodybuilders with their workouts, and be an assistant for cooking recipes.</p>
-        <p>Coming soon.</p>
-            
-        </div>
-        </>
-    );
-}
