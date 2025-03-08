3e4802893c21695890471637f2953731f4fae894
Rohan Marar <97607649+Rohan5manza@users.noreply.github.com>
2025-03-08 12:08:41 +0530

    Update Initiatives.tsx

--- src/app/components/Initiatives.tsx
+++ src/app/components/Initiatives.tsx
@@ -1,22 +1,27 @@
 "use client";
 import styles from "../page.module.css";
 
-export default function Initiatives() {
+export default function Products() {
     return (
-        <>
-            <h2 className={styles.heading}>Think Outside The Box.</h2>
-            <div>
-                {/* First Image */}
-                <div>
-                    <p>Under TOTB initiative, we make tech videos spanning multiple domains.</p>
-                    <p>This is our content-creation led growth engine to promote BulkBites, and to share our technical journey along the process in a fun way.</p>
-                    <p>Content includes YouTube videos, guides, tutorials, and books as educational resources.</p>
-                </div>
-                {/* Second Image */}
-                <div className={styles.imageContainer}>
-                    <img src="NewImage.png" className={styles.largeImage} alt="New Image"/>
+        <div className={styles.elements}>
+            <div className={styles.content}>
+                <h2 className={styles.heading}>
+                Think Outside the Box
+                </h2>
+                
+                <div className={styles.gridContainer}>
+                    <div className={styles.Item}>
+                        <img src="NewImage.png" className={styles.largeImage} alt="New Image"/>
+                        <p className={styles.Type}>Our initiative</p>
+                        <p className={styles.ProductName}>Content creation</p>
+                        <p className={styles.Description}>TOTB is our content-led growth engine to promote BulkBites and allow us to share our technical journey with everyone. It lets our creative juices flow!</p>
+                        <a className={styles.Status} href='https://youtube.com/@tensorbuilds?si=ytvcnRmFjcMXDAOi/'>Watch us on Youtube</a>
+                        <a className={styles.Status} href='https://tensorbuilds.substack.com/'>Subscribe to our monthly newsletter</a>
+                        <a className={styles.Status} href='https://www.instagram.com/tensor_builds/'>Follow us on Insta</a>
+                    </div>
+
                 </div>
             </div>
-        </>
+        </div>
     );
 }
