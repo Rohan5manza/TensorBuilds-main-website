97eccb40685a03fb58e6fa4f2c5984581cb75b46
Rohan Marar <97607649+Rohan5manza@users.noreply.github.com>
2025-03-08 12:12:03 +0530

    Update Innovations.tsx

--- src/app/components/Innovations.tsx
+++ src/app/components/Innovations.tsx
@@ -14,7 +14,6 @@ export default function Products() {
                         <p className={styles.Type}>Mobile app</p>
                         <p className={styles.ProductName}>Health and fitness</p>
                         <p className={styles.Description}>An upcoming computer vision app for food recognition and estimation of calories & macronutrients.Designed to help bodybuilders with their workouts, and assist with cooking recipes.</p>
-                        <a className={styles.Status} href='/'>Coming soon</a>
                     </div>
 
                 </div>
