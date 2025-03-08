797e5e29e27cee3f4d33959977d105618dd79458
Rohan Marar <97607649+Rohan5manza@users.noreply.github.com>
2025-03-08 11:57:00 +0530

    Update Innovations.tsx

--- src/app/components/Innovations.tsx
+++ src/app/components/Innovations.tsx
@@ -1,1 +1,25 @@
+"use client";
+import styles from '../page.module.css';
 
+export default function Products() {
+    return (
+        <div className={styles.elements}>
+            <div className={styles.content}>
+                <h2 className={styles.heading}>
+                BulkBites 
+                </h2>
+                
+                <div className={styles.gridContainer}>
+                    <div className={styles.Item}>
+                    <img src="infraedge.png" alt="Personal URL" className={styles.gridImage} />
+                        <p className={styles.Type}>Mobile app</p>
+                        <p className={styles.ProductName}>Health and fitness</p>
+                        <p className={styles.Description}>An upcoming computer vision app for food recognition and estimation of calories & macronutrients.Designed to help bodybuilders with their workouts, and assist with cooking recipes.</p>
+                        <a className={styles.Status} href='https://infraedge.tensorbuilds.tech/'>Access here</a>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    );
+}
